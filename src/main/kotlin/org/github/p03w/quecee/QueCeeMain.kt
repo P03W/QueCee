83367dc05b206ab2014220469c493163a706238f
P03W <P03W@users.noreply.github.com>
2021-04-11 19:24:59 -0400

    Init (1.0.0)

---
+++ src/main/kotlin/org/github/p03w/quecee/QueCeeMain.kt
@@ -0,0 +1,25 @@
+package org.github.p03w.quecee
+
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
+import net.minecraft.server.network.ServerPlayerEntity
+
+object QueCeeMain : ModInitializer{
+    override fun onInitialize() {
+        ServerTickEvents.START_WORLD_TICK.register {
+            it.players.forEach(::stripPlayerOfGuiItems)
+        }
+
+        ServerTickEvents.END_WORLD_TICK.register {
+            it.players.forEach(::stripPlayerOfGuiItems)
+        }
+    }
+
+    private fun stripPlayerOfGuiItems(player: ServerPlayerEntity) {
+        player.inventory.remove(
+            { stack -> stack.tag?.contains("QUECEE-DELETE") ?: false },
+            Int.MAX_VALUE,
+            player.playerScreenHandler.method_29281()
+        )
+    }
+}
---
+++ src/main/kotlin/org/github/p03w/quecee/api/gui/inventory/ItemActionMap.kt
@@ -0,0 +1,37 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.github.p03w.quecee.api.gui.inventory
+
+import net.minecraft.inventory.Inventory
+import net.minecraft.item.ItemStack
+import org.github.p03w.quecee.util.DualHashMap
+import org.github.p03w.quecee.util.GuiAction
+
+/**
+ * A simple wrapper that indexes `GuiAction`s and `ItemStack`s into slot numbers + some utility functions
+ */
+class ItemActionMap<T>(method: ItemActionMap<T>.() -> Unit) {
+    private val backingMap: DualHashMap<Int, GuiAction<T>, ItemStack> = DualHashMap()
+
+    init {
+        this.method()
+    }
+
+    fun runActionAt(slotId: Int, data: Int, state: T) {
+        if (backingMap.contains(slotId)) {
+            backingMap[slotId].first.invoke(data, state)
+        }
+    }
+
+    fun addEntry(slot: Int, stack: ItemStack, action: GuiAction<T>) {
+        backingMap.set(slot, action, stack)
+    }
+
+    fun copyIntoInventory(inv: Inventory) {
+        backingMap.forEach { slot, _, item -> inv.setStack(slot, item) }
+    }
+}
